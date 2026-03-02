d457eb7be412ce4eb561d75b019ba8cddd642fed
annisafathul <annisafathul18@gmail.com>
2026-03-02 20:50:34 +0700

    Belah ketupat 3

--- Luas Belah Ketupat.cpp
+++ Luas Belah Ketupat.cpp
@@ -15,3 +15,8 @@ float ProsedurHitungLuasBerparameter(float D1,float D2)
 {
     return 0.5 * D1 * D2; 
 }
+
+void ProsedurOutput()
+{
+    cout <<"Luas Ketupat :" << ProsedurHitungLuasBerparameter(D1, D2);
+}
