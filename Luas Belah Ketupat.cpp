d0cdcb1bfc92d692641a9febe832640c662db520
annisafathul <annisafathul18@gmail.com>
2026-03-02 20:46:24 +0700

    Belah ketupat 1

---
+++ Luas Belah Ketupat.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+float Luaas, D1, D2;
+
+void ProsedurInputData ()
+{
+    cout << "Masukkan Diagonal 1 : ";
+    cin >> D1;
+    cout << "Masukkan Diagonal 2 : ";
+    cin >> D2;
+}
