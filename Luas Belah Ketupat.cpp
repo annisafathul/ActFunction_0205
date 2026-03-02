3ed855054885a8008f5bac0fb7b4c3c76f353faf
annisafathul <annisafathul18@gmail.com>
2026-03-02 21:00:26 +0700

    Belah ketupat 1

--- Luas Belah Ketupat.cpp
+++ Luas Belah Ketupat.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
 using namespace std;
 
-float Luaas, D1, D2;
+float Luas, D1, D2;
 
-void ProsedurInputData ()
+void ProsedurInputData()
 {
     cout << "Masukkan Diagonal 1 : ";
     cin >> D1;
     cout << "Masukkan Diagonal 2 : ";
     cin >> D2;
-}
-
-float ProsedurHitungLuasBerparameter(float D1,float D2)
-{
-    return 0.5 * D1 * D2; 
-}
-
-void ProsedurOutput()
-{
-    cout <<"Luas Ketupat :" << ProsedurHitungLuasBerparameter(D1, D2);
 }
